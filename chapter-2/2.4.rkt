bd4062758a9552a56bcef3db457ad35748309d29
felang <langtao0415@outlook.com>
2020-10-22 15:06:43 +0800

    update

--- chapter-2/2.4.rkt
+++ chapter-2/2.4.rkt
@@ -97,3 +97,51 @@
 
 (define (make-from-mag-ang r a)
     (make-from-mag-ang-polar r a))
+
+; 2.4.3
+(define (install-rectangular-package)
+    (define (real-part z) (car z))
+    (define (imag-part z) (cdr z))
+    (define (make-from-real-imag x y) (cons x y))
+    (define (magnitude z)
+        (sqrt (+ (square (real-part z))
+                 (square (imag-part z)))))
+    (define (angle z)
+        (atan (imag-part z) (real-part z)))
+    (define (make-from-mag-ang r a)
+        (cons (* r (cos a)) (* r (sin a))))
+    
+    (define (tag x) (attach-tag 'rectangular x))
+    (put 'real-part '(rectangular) real-part)
+    (put 'imag-part '(rectangular) imag-part)
+    (put 'magnitude '(rectangular) magnitude)
+    (put 'angle '(rectangular) angle)
+    (put 'make-from-real-imag 'rectangular
+         (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'rectangular
+         (lambda (r a) (tag (make-from-mag-ang r a))))
+    'done)
+
+
+(define (apply-generic op . args)
+    (let ((type-tags (map type-tag args)))
+        (let ((proc (get op type-tags)))
+            (if proc
+                (apply proc (map contents args))
+                (error "No method for these types -- APPLY_GENERIC" (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z))
+
+; 2.73
+(define (deriv exp var)
+    (cond ((number? exp) 0)
+          ((variable? exp) (if (same-variable? exp var) 1 0))
+          (else ((get 'deriv (operator exp)) (operands exp)
+                                             var))))
+
+(put 'deriv '+ deriv)
+
+; (get 'deriv + '(x 1))
+(define (deriv s var)
+    (make-sum (deriv (addend exp) var)
+              (deriv (augend exp) var)))
