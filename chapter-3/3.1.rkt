1bb9c167ddeb6e2bd8d5f8846023fc61db9770e6
felang <langtao0415@outlook.com>
2020-10-23 18:10:16 +0800

    update

--- chapter-3/3.1.rkt
+++ chapter-3/3.1.rkt
@@ -6,3 +6,161 @@
         (begin (set! balance (- balance amount))
                balance)
         "Insufficient funds"))
+
+(define new-withdraw
+    (let ((balance 100))
+        (lambda (amount)
+            (if (>= balance amount)
+                (begin (set! balance (- balance amount))
+                       balance)
+                "Insufficient funds")))))
+
+(define (make-withdraw balance)
+    (lambda (amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                   balance)
+            "Insufficient funds")))
+
+(define (make-account balance)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                   balance)
+            "Insufficient funds"))
+    (define (deposit amount)
+        (set! balance (+ balance amount))
+        balance)
+    (define (dispatch m)
+        (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+    dispatch)
+
+; (define acc (make-accout 100))
+; ((acc 'withdraw) 50)
+
+; 3.1
+(define (make-accumulator int)
+    (lambda (x)
+        (begin (set! int (+ int x))
+        int)))
+
+; 3.2
+(define (make-monitored f)
+    (let ((i 0))
+        (lambda (m)
+            (cond ((eq? m 'how-many-calls?) i)
+                  (else (begin (set! i (+ i 1))
+                        (f m)))))))
+; (define s (make-monitored sqrt))
+; (s 100)
+; (s 25)
+; (s 'how-many-calls?)
+
+; 3.3
+(define (make-account balance secret)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                   balance)
+            "Insufficient funds"))
+    (define (deposit amount)
+        (set! balance (+ balance amount))
+        balance)
+    (define (dispatch s m)
+        (if (not (= s secret))
+            "Incorrect password"
+            (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              (else (error "Unknown request -- MAKE-ACCOUNT" m)))))
+    dispatch)
+
+; 3.4
+(define (make-account balance secret)
+    (let ((max-try 7)
+          (try-times 0))
+        (define (withdraw amount)
+            (if (>= balance amount)
+                (begin (set! balance (- balance amount))
+                    balance)
+                "Insufficient funds"))
+        (define (deposit amount)
+            (set! balance (+ balance amount))
+            balance)
+        (define (dispatch s m)
+            (if (eq? s secret)
+                (begin
+                    (set! try-times 0)
+                    (cond ((eq? m 'withdraw) withdraw)
+                          ((eq? m 'deposit) deposit)
+                          (else (error "Unknown request -- MAKE-ACCOUNT" m))))
+                (begin
+                    (set! try-times (+ try-times 1))
+                    (if (= try-times max-try)
+                        (call-the-cops)
+                        "Wrong Password"))))
+        dispatch))
+
+; 3.1.2
+(define rand
+    (let ((x random-init))
+        (lambda ()
+            (set! x (rand-update x))
+            x)))
+
+(define (estimate-pi trials)
+    (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+(define (cesaro-test)
+    (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+    (define (iter trials-remaining trials-passed)
+        (cond ((= trials-remaining 0)
+               (/ trials-passed trials))
+              ((experiment)
+               (iter (- trials-remaining 1) (+ trials-passed 1)))
+              (else
+               (iter (- trials-remaining 1) trials-passed))))
+    (iter trials 0))
+
+; 3.6
+(define rand
+    (let ((x random-init))
+        (define (dispatch message)
+            (cond ((eq? message 'generate)
+                   (begin (set! x (rand-update x))
+                          x))
+                  ((eq? message 'reset)
+                   (lambda (new-value) (set! x new-value)))))
+        dispatch))
+
+; 3.7
+(define (make-account balance secret)
+    (define (withdraw amount)
+        (if (>= balance amount)
+            (begin (set! balance (- balance amount))
+                   balance)
+            "Insufficient funds"))
+    (define (deposit amount)
+        (set! balance (+ balance amount))
+        balance)
+    (define (dispatch s m)
+        (if (not (= s secret))
+            "Incorrect password"
+            (cond ((eq? m 'withdraw) withdraw)
+              ((eq? m 'deposit) deposit)
+              (else (error "Unknown request -- MAKE-ACCOUNT" m)))))
+    dispatch)
+
+(define (make-joint account secret new-secret)
+    (lambda (s m)
+        (if (eq? s new-secret)
+            (account secret m)
+            "Incorrect password")))
+
+; 3.8
+(define f
+    (lambda (fv)
+        (set! f (secon-value) 0))
+        first-value)
---
+++ chapter-3/3.3.rkt
@@ -0,0 +1,125 @@
+; 3.12
+(define (append! x y)
+    (set-cdr! (last-pair x) y)
+    x)
+(define (append x y)
+    (if (null? x)
+        y
+        (cons (car x) (append (cdr x) y))))
+(define (last-pair x)
+    (if (null? (cdr x))
+        x
+        (last-pair (cdr x))))
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+(define z (append x y))
+z
+(cdr x)
+
+(define w (append! x y))
+w
+(cdr x)
+
+; 3.13
+(define (make-cycle x)
+    (set-cdr! (last-pair x) x)
+    x)
+(define z (make-cycle (list 'a 'b 'c)))
+
+; 3.14
+(define (mystery x)
+    (define (loop x y)
+        (if (null? x)
+            y
+            (let ((temp (cdr x)))
+                (set-cdr! x y)
+                (loop temp x))))
+    (loop x '()))
+
+(define v (list 'a 'b 'c 'd))
+(define w (mystery v))
+
+; 3.15
+
+; 3.16
+(define (count-pairs x)
+    (if (not (pair? x))
+        0
+        (+ (count-pairs (car x))
+           (count-pairs (cdr x))
+           1)))
+(define x1 (cons 1 2))
+(define three (cons x1 x1)) ;3
+(define four (cons 1 (cons x1 x1))) ;4
+(define seven (cons 1 （cons x1 x2)) ;7
+; 3.17
+(define (count-pairs x)
+    (let ((encountered '()))
+        (define (helper x)
+            (if (or (not (pair? x)) (memq x encountered))
+                0
+                (begin
+                    (set! encountered (cons x encountered))
+                    (+ (helper (car x))
+                       (helper (cdr x))
+                       1))))
+        (helper x)))
+; 3.18
+(define (cycle? x)
+    (let ((var '()))
+        (define (loop x)
+            (if (not pair? x)
+                false
+                (if (memq x var)
+                    true
+                    (begin
+                        (set! var (cons x var))
+                        (or (loop (car x))
+                            (loop (cdr x)))))))
+        (loop x)))
+; 3.19
+
+; 3.20
+
+
+
+; 3.3.2 queue
+(define (front-ptr queue) (car queue))
+(define (rear-prt queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+    (if (empty-queue? queue)
+        (error "FRONT called with an empty queue" queue)
+        (car (front-prt queue))))
+
+(define (insert-queue! queue item)
+    (let ((new-pair (cons item '())))
+        (cond ((empty-queue? queue)
+               (set-front-ptr! queue new-pair)
+               (set-rear-ptr! queue new-pair)
+               queue)
+              (else
+               (set-cdr! (rear-ptr queue) new-pair)
+               (set-rear-ptr! queue new-pair)
+               queue))))
+
+(define (delete-queue! queue)
+    (cond ((empty-queue? queue)
+           (error "DELETE! called with an empty queue" queue))
+          (else
+           (set-front-ptr! queue (cdr (front-ptr queue)))
+           queue)))
+; 3.21
+(define (print-queue queue)
+    (display (front-ptr queue)))
+
+; 3.22
+(define (make-queue)
+    (let ((front-ptr '())
+          (rear-ptr '()))
+        ))
