f50566cff189432a01a757e0e153ffa57e973bf7
felang <langtao0415@outlook.com>
2020-10-26 19:25:07 +0800

    update

--- chapter-3/3.3.rkt
+++ chapter-3/3.3.rkt
@@ -594,3 +594,9 @@ w
 (define carry (make-wire))
 
 (probe 'sum sum)
+(probe 'carry carry)
+(half-adder input-1 input-2 sum carry)
+(set-signal! input-1 1)
+(propagate)
+(set-signal! input-2 1)
+(propagate)
