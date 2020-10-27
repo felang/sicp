93e4b041f83d3f67b9bf73a855c790707f4484dc
felang <langtao0415@outlook.com>
2020-10-27 18:22:01 +0800

    update

---
+++ chapter-3/3.5.rkt
@@ -0,0 +1,104 @@
+; stream
+(define the-empty-stream '())
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
+(define (stream-car stream) (car stream))
+(define (stream-null? stream) (null? stream))
+(define (stream-cdr stream) (force (cdr stream)))
+
+(define (memo-proc proc)
+    (let ((already-run? false) (result false))
+        (lambda ()
+            (if (not already-run?)
+                (begin (set! result (proc))
+                       (set! already-run? true)
+                       result)
+                result))))
+
+(define-syntax delay
+  (syntax-rules ()
+    ((_ exp) (memo-proc (lambda () exp)))))
+
+(define (force delayed-object)
+    (delayed-object))
+
+(define (stream-ref s n)
+    (if (= n 0)
+        (stream-car s)
+        (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+    (if (stream-null? s)
+        the-empty-stream
+        (cons-stream (proc (stream-car s))
+                     (stream-map proc (stream-cdr s)))))
+
+(define (display-stream s)
+    (stream-for-each display-line s))
+(define (display-line x)
+    (newline)
+    (display x))
+(define (stream-enumerate-interval low high)
+    (if (> low high)
+        the-empty-stream
+        (cons-stream
+         low
+         (stream-enumerate-interval (+ low 1) high))))
+(define (stream-for-each proc s)
+    (if (stream-null? s)
+        'done
+        (begin (proc (stream-car s))
+               (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+    (cond ((stream-null? stream) the-empty-stream)
+          ((pred (stream-car stream))
+           (cons-stream (stream-car stream)
+                        (stream-filter pred
+                                       (stream-cdr stream))))
+          (else (stream-filter pred (stream-cdr stream)))))
+
+
+(define (show x)
+    (display-line x)
+    x)
+
+(define x (stream-map show (stream-enumerate-interval 0 100)))
+
+(stream-ref x 5)
+(stream-ref x 7)
+; 3.50
+(define (stream-map proc . argstreams)
+    (if (null? (car argstreams))
+        '()
+        (cons-stream
+            (apply proc
+                   (map (lambda (s) (stream-car s))
+                        argstreams))
+            (apply stream-map
+                   (cons proc (map (lambda (s) (stream-cdr s))
+                                   argstreams))))))
+
+; 3.51
+(define (show x)
+    (display-line x)
+    x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+(stream-ref x 5)
+(stream-ref x 7)
+
+; 3.52
+(define sum 0)
+(define (accum x)
+    (set! sum (+ x sum))
+    sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0))
+                         seq))
+
+(stream-ref y 7)
+(display-stream z)
